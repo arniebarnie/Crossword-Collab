e142e23bc0fdd490c48ec307c4c0f7ba4e2a700b
Arnav Rastogi <arnav.rastogi101@gmail.com>
2025-02-17 02:07:16 -0600

    compilation fix

--- crossword-collab/src/__tests__/utils/firebase-test-utils.ts
+++ crossword-collab/src/__tests__/utils/firebase-test-utils.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
-import { beforeAll, afterAll, afterEach } from '@testing-library/react'
+import { connectFirestoreEmulator } from 'firebase/firestore'
+import { beforeAll, afterAll, afterEach } from '@jest/globals'
+import { app, auth, db } from '@/firebase/config'
 
 const testConfig = {
   apiKey: 'fake-api-key',
@@ -12,6 +14,7 @@ export const setupEmulators = () => {
   const app = initializeApp(testConfig)
   const auth = getAuth(app)
   connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true })
+  connectFirestoreEmulator(db, '127.0.0.1', 8080)
   return { app, auth }
 }
 
